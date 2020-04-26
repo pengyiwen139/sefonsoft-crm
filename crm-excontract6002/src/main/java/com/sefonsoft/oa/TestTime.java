5009e5606bc938e6432784003164cb1b86edb582
pengyiwen <pengyiwen139@outlook.com>
2020-04-27 07:11:45 +0800

    跟新文档 gateway gateway测试成功

---
+++ crm-excontract6002/src/main/java/com/sefonsoft/oa/TestTime.java
@@ -0,0 +1,12 @@
+package com.sefonsoft.oa;
+
+import java.time.ZonedDateTime;
+
+public class TestTime {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+
+}
